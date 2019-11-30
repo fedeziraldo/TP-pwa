b6a385d754cc91bd1b41fb68bb2304afb22d8016
efabbiani <enzofab91@hotmail.com>
2019-11-30 00:04:20 -0300

    Quienes somos completo

--- controllers/quienes.js
+++ controllers/quienes.js
@@ -3,8 +3,14 @@ const quienesModel = require("../models/quienesModel");
 module.exports = {
     saveQuienes: async function (req, res, next) {
         try {
-            //ToDo: hay que poner fecha de eliminado al activo y dar de alta el nuevo
-            var data = await quienesModel.create({ titulo: req.body.titulo, descripcion: req.body.descripcion, contacto: req.body.contacto, fAlta: new Date()});
+            var cant = await quienesModel.countDocuments();
+            var data;
+
+            if (cant == 0)
+                data = await quienesModel.create({ titulo: req.body.titulo, descripcion: req.body.descripcion, contacto: req.body.contacto, fAlta: new Date()});
+            else
+                data = await quienesModel.findOneAndUpdate({}, { titulo: req.body.titulo, descripcion: req.body.descripcion, contacto: req.body.contacto })
+            
             res.json({ status: "success", message: "Quienes added successfully!!!", data: data });
         } catch (err) {
             console.log(err);
@@ -14,7 +20,9 @@ module.exports = {
 
   getQuienes: async function (req, res, next) {
     try {
-      var data = await quienesModel.find({ fEliminado: {$ne: null}});
+      //var data = await quienesModel.find({ fEliminado: {$ne: null}});
+      var data = await quienesModel.findOne();
+      console.log(data);
       res.json({ status: "success", data: data });
     } catch (err) {
       console.log(err);
